9ffe36b6a823a3a3ba4945570c6ef7ba2268b3ea
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-06-08 10:31:01 -0400

    Update Debug1.rb

--- Week1/Debug1.rb
+++ Week1/Debug1.rb
@@ -1,3 +1,16 @@
+#--------------------------------------------------------------------------
+# 
+# Script Name: Debug1.rb
+# Version:     1.3
+# Author:      Jaime V. Rodriguez
+# Date:        08 June 2020
+# Assignment #: Assignment 1.3
+# 
+# Description: This Ruby script has purposes inserted "bugs" in order 
+#              for fellow students to practice debuggin a program. 
+# 
+#--------------------------------------------------------------------------
+
 print "Enter your name: "
 
 #miss "D" should read STDIN.gets
